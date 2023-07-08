613b4d77be21910a6aae9374617cd53182cc715e
Amalya1278 <138888323+Amalya1278@users.noreply.github.com>
2023-07-08 22:17:32 +0400

    sym.c

--- sym.c
+++ sym.c
@@ -2,23 +2,20 @@
 int main()
 {
 	char sym;
-	printf("Enter the sym\n");
-	scanf("%c",&sym);
 	
 	do
-	{		if (sym<='A' && sym >='Z')
+	{	scanf("%c",&sym);
+		if (sym>='A' && sym <='Z')
 		{
-			printf("Enter right variable");
-			scanf("%c",&sym);
+			sym=sym+32;
+			
 		}
+	 else break;
+	 printf("%c",sym);
 		
 	}
 	while (sym>='A' && sym<='Z');
 	
-		char low=sym+32;
-		printf("%c is the lowercase",low);
-	
-
 	return 0;
 }
 
